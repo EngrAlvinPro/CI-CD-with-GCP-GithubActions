1ef8d867d1b77323b155806e5ccc18ab46b76cbc
EngrAlvin <engralvinpro@gmail.com>
2025-04-01 23:12:40 +0800

    Improved UI

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CI/CD with GitHub Actions and Google Cloud!"
+    #return "Hello, CI/CD with GitHub Actions and Google Cloud!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
